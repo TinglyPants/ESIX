085fba5920edd5b0687cdddbf3f4a23bc28a9b5c
Matty Chalk <chalkytangfastics@gmail.com>
2023-11-09 20:12:16 +0000

    /e621 first iteration.

---
+++ commands/e621.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('discord.js')
+const { responses } = require('responses.json')
+
+function choose(array){
+    let index = Math.floor(Math.random() * array.length)
+    return array[index]
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("e621")
+    .setDescription('1 in 10000 chance of a random, unfiltered e621 image. Use at own risk.'),
+    async execute(interaction) {
+        try {
+            let postNum = Math.floor(Math.random() * 4_402_916)
+            let chanceNum = Math.floor(Math.random() * 10000)
+
+            if (chanceNum == 621){
+                await interaction.reply(`Here you go then...\nhttps://e621.net/posts/${postNum}`)
+            }
+            else{
+                await interaction.reply(choose(responses))
+            }
+        }
+        catch (error){
+            console.log(error)
+            await interaction.reply("Something broke. Maybe its a sign to stop.")
+        }
+    }
+}
