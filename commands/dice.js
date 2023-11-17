41c58e96bd53ad3516ea50a0778c89c0769a0b05
TinglyPants <chalkytangfastics@gmail.com>
2023-11-17 20:55:48 +0000

    Added /roll

---
+++ commands/dice.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("roll")
+    .setDescription('Will roll the dice!')
+    .addIntegerOption(option => 
+        option
+            .setName('count')
+            .setRequired(true)
+            .setDescription('Number of sides')
+    ),
+    async execute(interaction) {
+        const count = interaction.options.getInteger('count')
+        num = Math.floor(Math.random() * count) + 1
+        await interaction.reply(`Roll: ${num}`)
+    }
+}
