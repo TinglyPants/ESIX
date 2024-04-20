2d3e81c2d41c86444114528260d6511f3df05a01
TinglyPants <chalkytangfastics@gmail.com>
2024-04-20 19:57:03 +0100

    Updated esix post count

--- commands/e621Commands/e621.js
+++ commands/e621Commands/e621.js
@@ -8,7 +8,7 @@ module.exports = {
         .setDescription('1 in 1,000,000 chance to send a random e621 image'),
     async execute(interaction) {
         try {
-            let postNum = Math.floor(Math.random() * 4_402_916)
+            let postNum = Math.floor(Math.random() * 4_732_269)
             let chanceNum = Math.floor(Math.random() * 100000)
 
             if (chanceNum == 621) {
